9cf2bf56b4dbcabd03182a2a4350706ee9db0d48
Tinnakorn <semjinwu@gmail.com>
2023-01-18 17:55:06 +0700

    1

--- st.py
+++ st.py
@@ -1,2 +1,11 @@
 import streamlit as st
-st.write("hello")
+from keras.models import load_model
+import matplotlib.pyplot as plt
+from PIL import Image
+import cv2
+
+model=load_model('weights_bestv5.hdf5')
+uploaded_file = st.file_uploader("Choose a file")
+if uploaded_file is not None:  
+    st.write(uploaded_file)
+    print(uploaded_file)
